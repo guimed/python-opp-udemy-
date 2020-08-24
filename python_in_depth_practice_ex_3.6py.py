0f2b2fc24ef77eebd4d49b85cb82e0e4a6b8cd89
Guilherme de Medeiros <67030281+guimed@users.noreply.github.com>
2020-08-24 17:55:31 -0300

    Add files via upload

---
+++ python_in_depth_practice_ex_3.6py.py
@@ -0,0 +1,28 @@
+class Stack:
+
+    Max_Size = 10
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def push(self, item):
+        if self.size == Stack.Max_Size:
+            raise RuntimeError('Stack is full')
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise RuntimeError('Stack is empty')
+        return self.items.pop()
+
+    def display(self):
+        print(self.items)
+           
+
+        
