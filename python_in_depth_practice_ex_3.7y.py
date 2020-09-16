0bf53296bcaeac3ce25dbc630d555375f1777042
Guilherme de Medeiros <67030281+guimed@users.noreply.github.com>
2020-09-16 18:45:06 -0300

    Add files via upload

---
+++ python_in_depth_practice_ex_3.7y.py
@@ -0,0 +1,17 @@
+class BankAccount:
+
+    bank_name = 'Itaú, República, SP'
+
+    def __init__(self, name, balance=0, bank=bank_name):
+        self.name = name
+        self.balance = balance
+        self.bank = bank
+
+    def display(self):
+        print(self.name, self.balance, self.bank)
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def deposit(self, amount):
+        self.balance += amount
